58bc8ba9ee3cc8aa69ac05da6229e008bfaeaf57
maoz8153 <maoz8153@gmail.com>
2015-03-28 02:22:23 +0300

    flush probability

--- Probability.py
+++ Probability.py
@@ -7,7 +7,7 @@ def deck_information(deck):
     for card in deck:
         num_value_cards[14 - card.value] += 1
         num_suit_cards[card.suit_index] += 1
-    return num_suit_cards, num_value_cards
+    return (num_suit_cards, num_value_cards)
 
 def value_probabilty(deck):
      return [(14 - index, frequency) for index, frequency in enumerate(deck) if frequency]
@@ -19,16 +19,16 @@ def flush_available(board, hand):
     cards_in_play.extend(board)
     cards_in_play.extend(hand)
     for card in cards_in_play:
-        if card.suit in suits:
-            suits[card.suit] += 1
+        if card.suit_index in suits:
+            suits[card.suit_index] += 1
         else:
-            suits[card.suit] == 1
+            suits[card.suit_index] == 1
 
     suits = sorted(suits.items(), key=lambda x: x[1], reverse=True)
     if len(cards_in_play) == 5 and suits[0][1] > 2:
-        return [suits[0][0], suits[0][1]]
+        return {suits[0][0] : suits[0][1]}
     elif len(cards_in_play) == 6 and suits[0][1] > 3:
-        return [suits[0][0], suits[0][1]]
+        return {suits[0][0] : suits[0][1]}
     else:
         return None
 
@@ -40,9 +40,23 @@ def flot_probabilty(board, hand):
     deck = holdem_gen.gen_deck_without_cards(out_cards)
 
 def flush_probabilty(board, hand):
+    suit_index_dict = {"s": 0, "c": 1, "h": 2, "d": 3}
     is_flush = flush_available(board,hand)
     if is_flush is not None:
         deck = holdem_gen.gen_deck_without_cards(board,hand)
+        suit_cards, values_cards  = deck_information(deck)
+        number_of_cards = 5 - is_flush[0][0]
+        if number_of_cards == 2 :
+            prob = suit_cards[is_flush[0][0]]*(suit_cards[is_flush[0][0]] - 1)/float(len(deck)* (len(deck) - 1))
+        if number_of_cards == 1
+            prob = suit_cards[is_flush[0][0]]/float(len(deck))
+        return prob
+    else:
+        return 0
+
+
+
+
 
 
 
