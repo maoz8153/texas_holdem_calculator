060040c569f8caa3aeb602281dcb8c8fec35a550
maoz8153 <maoz8153@gmail.com>
2015-05-09 18:50:34 +0300

    added gulp with template

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 var express = require('express'),
-request=require("request");
-var app  = module.exports = express();
+    request=require("request");
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
 
 var options = {
     host : 'http://127.0.0.1',
@@ -9,34 +13,33 @@ var options = {
     method : 'POST'
 };
 
-app.configure(function() {
-  app.set('views', __dirname + '/server/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(stylus.middleware(
-    {
-      src: __dirname + '/public',
-      compile: compile
-    }
-  ));
-  app.use(express.static(__dirname + '/public'));
-});
 
 
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-});
 
-app.get('*', function(req, res) {
-  res.render('index');
-});
 
-request.get(options,function(error,response,body){
+
+
+/*request.get(options,function(error,response,body){
     if(error){
         console.log(error);
     }else{
         console.log(response);
     }
+});*/
+
+app.use(express.static(__dirname + '/views'));
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/',function(req,res){
+
+          res.render('index');
+
 });
 
+
+var server=app.listen(3000,function(){
+console.log("Express is running on port 3000");
+});
