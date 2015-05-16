ae2ba2fac7a966fa7f9219cd562cd17b5ef0137a
maoz8153 <maoz8153@gmail.com>
2015-05-16 21:39:34 +0300

    change modal

--- server/views/thumbs.js
+++ server/views/thumbs.js
@@ -22,6 +22,7 @@ app.controller('MainCtrl', function($scope, Cards) {
 
 
             $scope.playerCards = [];
+            $scope.selected = '';
             $scope.boardCards = [];
             $scope.selected_form = false;
             $scope.select_cards = false;
@@ -38,53 +39,55 @@ app.controller('MainCtrl', function($scope, Cards) {
             };
 
         })
-        app.controller('ModalDemoCtrl', function($scope, $modal, $log, Cards) {
-            $scope.items = [
-                'item1',
-                'item2',
-                'item3'
-            ];
-
-            $scope.card_display = Cards;
-            $scope.animationsEnabled = true;
-            $scope.selectedNumberOfPlayers = '';
-            $scope.selectedNumberOfCards = '';
-            $scope.open = function(size, card) {
-                var modalInstance = $modal.open({
-                    animation: $scope.animationsEnabled,
-                    templateUrl: 'cards_frame.html',
-                    windowClass: 'app-modal-window',
-                    scope: $scope,
-                    controller: 'ModalInstanceCtrl',
-                    size: size,
-                    resolve: {
-                        card_display: function() {
-                            return $scope.card_display;
-                        }
-                    }
-                });
-                modalInstance.result.then(function(selectedItem) {
-                    $scope.selected = selectedItem;
-                }, function() {
-                    $log.info('Modal dismissed at: ' + new Date());
-                });
-            };
-            $scope.toggleAnimation = function() {
-                $scope.animationsEnabled = !$scope.animationsEnabled;
-            };
+app.controller('ModalDemoCtrl', function($scope, $modal, $log, Cards) {
+    $scope.items = [
+        'item1',
+        'item2',
+        'item3'
+    ];
+    $scope.selectedCard = '';
+    $scope.card_display = Cards;
+    $scope.animationsEnabled = true;
+    $scope.selectedNumberOfPlayers = '';
+    $scope.selectedNumberOfCards = '';
+    $scope.open = function(size, card) {
+        $scope.selectedCard = card;
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'cards_frame.html',
+            windowClass: 'app-modal-window',
+            scope: $scope,
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                card_display: function() {
+                    return $scope.card;
+                }
+            }
         });
-        // Please note that $modalInstance represents a modal window (instance) dependency.
-        // It is not the same as the $modal service used above.
-        app.controller('ModalInstanceCtrl', function($scope, $modalInstance, card_display, Cards) {
-            $scope.card_display.shaps = Cards;
-            $scope.card_display = card_display;
-            $scope.selected = {
-                card_display: $scope.card_display[0]
-            };
-            $scope.ok = function() {
-                $modalInstance.close($scope.selected.card_display);
-            };
-            $scope.cancel = function() {
-                $modalInstance.dismiss('cancel');
-            };
+        modalInstance.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
         });
+    };
+    $scope.toggleAnimation = function() {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+});
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+app.controller('ModalInstanceCtrl', function($scope, $modalInstance, card_display, Cards) {
+    $scope.card_display.shaps = Cards;
+    //$scope.card_display = card_display;
+    $scope.selected = {
+        selectedCard: $scope.card_display[0]
+    };
+    $scope.ok = function(selectedCard) {
+        $scope.card = selectedCard;
+        $modalInstance.close();
+    };
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+});
